eab3718e449e147f114d85709c17a9c3c976a3c5
ships97 <shiprasingh4796@gmail.com>
2023-02-19 07:41:46 +0530

    progress

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 /* Configuration */
 
